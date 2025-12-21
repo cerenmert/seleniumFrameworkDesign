c843e51fe29d3d9cb36420e176c62f0fcebb4214
Ceren <ceren1481@hotmail.com>
2025-12-22 00:31:58 +0300

    implemented IRetryAnalyzer interface to rerun failed case

---
+++ src/test/java/testComponents/Retry.java
@@ -0,0 +1,18 @@
+package testComponents;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class Retry implements IRetryAnalyzer {
+    private int count = 0;
+    private static final int maxTry = 1; // Run the failed test 1 more time
+
+    @Override
+    public boolean retry(ITestResult iTestResult) {
+        if (count < maxTry) {
+            count++;
+            return true;
+        }
+        return false;
+    }
+}
--- src/test/java/tests/ErrorValidationTest.java
+++ src/test/java/tests/ErrorValidationTest.java
@@ -3,6 +3,7 @@ package tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import testComponents.BaseTest;
+import testComponents.Retry;
 
 public class ErrorValidationTest extends BaseTest {
     String email = "ceren1481@gmail.com";
@@ -14,5 +15,11 @@ public class ErrorValidationTest extends BaseTest {
         landingPage.loginToApp(email, invalidPassword);
         String errorMessage = landingPage.getErrorMessageForInvalidPassword();
         Assert.assertEquals(errorMessage, invalidPasswordMessage);
+    }
+    @Test(retryAnalyzer = Retry.class)
+    public void loginErrorRetry() {
+        landingPage.loginToApp(email, invalidPassword);
+        String errorMessage = landingPage.getErrorMessageForInvalidPassword();
+        Assert.assertEquals(errorMessage, "this test is failed, retrying... ");
     }
 }
